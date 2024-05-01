7e8a498fed5dbf1f9c390e6d0c394815bf81411e
Luis Emidio <luisfuturist@gmail.com>
2024-05-01 14:43:00 -0300

    chore: fix changelog bump script

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -3,10 +3,13 @@
 # Define the filename of your changelog file
 changelog_file="../CHANGELOG.md"
 
+# Get the version from gradle.properties
+version=$(grep -oP 'version=\K.*' gradle.properties)
+
 # Get today's date in the format YYYY-MM-DD
 today_date=$(date +%F)
 
 # Replace the [Unreleased] section with today's date
-sed -i "s/\[Unreleased\]/[$today_date]/" "$changelog_file"
+sed -i "s/\[Unreleased\]/[$version] - $today_date/" "$changelog_file"
 
 echo "Changelog updated with today's date: $today_date"
